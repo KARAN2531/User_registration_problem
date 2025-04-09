c803aa98808dfc6a6df0da241c0994f850e42fd9
Karan Gangwani <kg4727@srmist.edu.in>
2025-04-09 11:50:29 +0530

    Completed use_case_2

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -11,3 +11,12 @@ def valid_first_name(first_name):
     else:
         return "Invalid: minimum 3 characters are required"
 
+
+# UC2 - As a User need to enter a valid Last Name
+# Rule: Last name starts with Cap and has minimum 3 characters
+
+def valid_last_name(last_name):
+    if re.match(r"^[A-Z][a-z]{2,}$", last_name):
+        return True
+    else:
+        return "Invalid: minimum 3 characters are required"
